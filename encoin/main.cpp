97d750556cd14400ceec4565454b658cb18bb4b0
nicro <dev.nicro@gmail.com>
2021-11-21 23:45:58 +0100

    refact(cli): Some improvements on basic CLI interface

--- encoin/main.cpp
+++ encoin/main.cpp
@@ -6,18 +6,39 @@
 #include <cxxopts.hpp>
 
 using namespace encoin;
-using argument_parser = cxxopts::Options;
+
+inline cxxopts::ParseResult cmdopts;
+
+template <class T >
+T getopt (const std::string &opt)
+{
+    if (cmdopts.count(opt))
+    {
+        return cmdopts[opt].as<T>();
+    }
+    std::cerr << opt << " was not provided" << std::endl;
+    exit(-1);
+}
+
+void undefopt(const std::string &opt) 
+{
+    std::cerr << opt << " is an undefined parameter" << std::endl;
+    exit(-1);
+}
 
 int main(int argc, char **argv)
 {
-    argument_parser parser("encoin", "A simple cryptocurrency");
+    cxxopts::Options parser("encoin", "A simple cryptocurrency");
     parser.add_options()
-        ("n,node", "Type of node", cxxopts::value<std::string>())
+        ("m,mode", "Mode type", cxxopts::value<std::string>())
+        ("c,cmd", "Command type", cxxopts::value<std::string>())
+        ("o,opt", "Option type", cxxopts::value<std::string>())
+        ("v,value", "Value type", cxxopts::value<std::string>())
         ("h,help", "Print usage");
     
 
-    parser.parse_positional({"node"});
-    auto cmdopts = parser.parse(argc, argv);
+    parser.parse_positional({"mode", "cmd", "opt", "value"});
+    cmdopts = parser.parse(argc, argv);
 
     if (cmdopts.count("help"))
     {
@@ -25,22 +46,55 @@ int main(int argc, char **argv)
         exit(0);
     }
 
-    if (cmdopts.count("node")) 
+    std::string mode_type = getopt<std::string>("mode");
+    if (mode_type == "mine") 
     {
-        std::string node_type = cmdopts["node"].as<std::string>();
-        if (node_type == "miner") 
-        {
-            std::cout << "miner mode selected" << std::endl;
-            exit(0);
-        } 
-        else if (node_type == "wallet") 
+        std::cout << "mining mode selected" << std::endl;
+        exit(0);
+    }
+    else if (mode_type == "wallet") 
+    {
+        std::string cmd_type = getopt<std::string>("cmd");
+        if (cmd_type == "address") 
         {
             wallet wallet;
             auto &&address = wallet.get_active_address();
             std::cout << "Your current address is " << address << std::endl;
             exit(0);
         }
+        else if (cmd_type == "balance") 
+        {
+                blockchain chain;
+                wallet wallet;
+                auto &&address = wallet.get_active_address();
+                std::cout << "Your current balance is " << chain.get_balance(address) << std::endl;
+                exit(0);
+        }
+        else if (cmd_type == "send")
+        {
+            std::cout << "send cmd selected" << std::endl;
+            exit(0);
+        }
+        else undefopt(cmd_type);
     }
+    else if (mode_type == "settings") 
+    {
+        std::string cmd_type = getopt<std::string>("cmd");
+        if (cmd_type == "set") 
+        {
+            std::string opt = getopt<std::string>("opt");
+            std::string value = getopt<std::string>("value");
+            std::cout << "setting " << opt << " to " << value << std::endl;
+            exit(0);
+        }
+        else if (cmd_type == "get")
+        {
+            std::string opt = getopt<std::string>("opt");
+            std::cout << opt << " equals " << "" << std::endl;
+            exit(0);
+        }
+    }
+    else undefopt(mode_type);
 
     return 0;
 }
